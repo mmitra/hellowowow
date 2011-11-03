944496827768ceca2e32077ffad28daa62a29394
Monty Mitra <mmitra@engineyard.com>
2011-11-02 17:09:08 -0700

    rest of it

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hellowowow::Application.config.secret_token = '503397d40a60527e4ad60061ee043d539c4b2a5cef86c025379bdc98bc2f18382579611185890971d36e60e06f78c4187a5c126e78a90144229ccbfa13040ef0'
